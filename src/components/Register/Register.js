7344b9e75a083db0fdbb3b9dd9e7800bc4c40310
Rio Cantre <smittenaf301icu@gmail.com>
2021-09-28 21:24:54 +0200

    Updated files

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Register = ({ onRouteChange }) => {
   return (
-    <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+    <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center white'>
       <main className='pa4 black-80'>
-        <form className='measure'>
+        <div className='measure'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
             <legend className='f2 fw6 ph0 mh0'>Register</legend>
             <div className='mt3'>
@@ -49,7 +49,7 @@ const Register = ({ onRouteChange }) => {
               value='Register'
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
