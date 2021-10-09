7315440c67201598c6050f513e33bbb3cabcfcb6
Rio Cantre <smittenaf301icu@gmail.com>
2021-10-09 22:51:01 +0200

    Update app

--- src/App.js
+++ src/App.js
@@ -26,24 +26,24 @@ const particleOptions = {
   }
 };
 
-
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      },
-    };
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -98,6 +98,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
         this.displayFacebox(this.calculateFaceLocation(response))
       })
@@ -108,7 +109,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
